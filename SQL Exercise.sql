7566cb3f4cd2666aba63d86946dd272f414e5f62
Sel007 <selina.08225.1@gmail.com>
2020-07-21 12:13:37 -0400

    Finished Challenges Up To Joins

---
+++ SQL Exercise.sql
@@ -0,0 +1,8 @@
+-- SQL Challenges
+SELECT * FROM Products;
+SELECT * FROM products Where Price = 1400;
+SELECT * FROM products Where Price BETWEEN 11.99 AND 13.99;
+SELECT * FROM PRODUCTS WHERE Not Price = 11.99;
+SELECT * FROM PRODUCTS order by Price Desc;
+SELECT * FROM EMPLOYEES WHERE MiddleInitial IS NULL OR MiddleInitial = ' ';
+SELECT DISTINCT Price from products;
